544d433b1b39f042bc3ef10a5c60c68a072efab8
Severin Ibarluzea <seve700@gmail.com>
2026-07-11 22:32:07 -0700

    Use manifold-2d from npm (#63)

--- tests/dependency-tree.test.ts
+++ tests/dependency-tree.test.ts
@@ -3,9 +3,7 @@ import manifoldPackageJson from "../node_modules/@tscircuit/manifold-2d/package.
 import packageJson from "../package.json"
 
 test("manifold does not add transitive production dependencies", () => {
-  expect(packageJson.dependencies["@tscircuit/manifold-2d"]).toBe(
-    "https://jscdn.tscircuit.com/@tscircuit/manifold-2d/0.0.4.tgz",
-  )
+  expect(packageJson.dependencies["@tscircuit/manifold-2d"]).toBe("^0.0.6")
   const manifest = manifoldPackageJson as {
     dependencies?: Record<string, string>
   }
